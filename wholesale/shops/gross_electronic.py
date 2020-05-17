a8c45d0d5d66aac7978b535c6cf20f7b26e75e4e
cxan96 <c.p.xanten@gmail.com>
2020-05-17 17:06:41 +0200

    added login for gross_electronic

--- wholesale/shops/gross_electronic.py
+++ wholesale/shops/gross_electronic.py
@@ -1,6 +1,31 @@
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import re
+
+
+base_url = "https://www.gross-electronic.de/"
+
+
+def get_hidden_login_field_data(login_html):
+    """Returns a dictionary of all the hidden fields in the login form."""
+
+    bs = BeautifulSoup(login_html, "html.parser")
+    login_form = bs.find("form", attrs={"name": "formular_login"})
+    hidden_fields = login_form.find_all("input", type="hidden")
+    result = {}
+    for cur_hidden_field in hidden_fields:
+        result[cur_hidden_field["name"]] = cur_hidden_field["value"]
+    return result
+
+
+def get_login_action_url(login_html):
+    """Returns the action url from the login form."""
+
+    bs = BeautifulSoup(login_html, "html.parser")
+    login_form = bs.find("form", attrs={"name": "formular_login"})
+    return urljoin(base_url, login_form["action"])
 
 
 class GrossElectronic:
@@ -8,25 +33,52 @@ class GrossElectronic:
         self.username = username
         self.password = password
         self.request_session = requests.Session()
-        self.user_agent = UserAgent().random
-        self.login_url = "https://www.gross-electronic.de/"
+        self.default_headers = {"User-Agent": UserAgent().random}
 
     def get_login_html(self):
-        """Returns a dictionary of all the prepopulated fields in the login form."""
-        res = self.request_session.get(
-            self.login_url, headers={"User-Agent": self.user_agent}
-        )
+        """Returns the html code of the login page."""
+        res = self.request_session.get(base_url, headers=self.default_headers)
 
         if not res.ok:
             raise Exception(
-                f"Could not connect to url {self.login_url}. "
+                f"Could not connect to url {base_url}. "
                 f"Status code: {res.status_code}, reason: {res.reason}"
             )
 
         return res.text
 
-    def get_login_form_data(self, login_html):
-        pass
+    def login(self, login_html):
+        hidden_field_data = get_hidden_login_field_data(login_html)
+        request_body = {
+            **hidden_field_data,
+            "userBez": self.username,
+            "userPasswort": self.password,
+        }
+        url = get_login_action_url(login_html)
 
-    def get_csv(self):
-        pass
+        res = self.request_session.post(
+            url, data=request_body, headers=self.default_headers
+        )
+
+        if not res.ok:
+            raise Exception(
+                f"Could not login at url {url}. "
+                f"Status code: {res.status_code}, reason: {res.reason}"
+            )
+
+        # The website performs a client side redirect using Javascript.
+        # Simulate this by parsing the target url using a regular expression.
+
+        url_finder = re.compile(".*'(?P<url>.*)'.*")
+        match = re.match(url_finder, res.text)
+        target_url = urljoin(base_url, match.group("url"))
+
+        res = self.request_session.get(target_url, headers=self.default_headers)
+
+        if not res.ok:
+            raise Exception(
+                f"Error after login redirect to {url}. "
+                f"Status code: {res.status_code}, reason: {res.reason}"
+            )
+
+        return res.text
