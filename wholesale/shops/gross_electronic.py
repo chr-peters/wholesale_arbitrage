8ba58f9a6af17a196c55be3ae347d41083909588
cxan96 <c.p.xanten@gmail.com>
2020-05-19 15:56:42 +0200

    completed .csv retrieval for gross_electronic

--- wholesale/shops/gross_electronic.py
+++ wholesale/shops/gross_electronic.py
@@ -3,6 +3,8 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
+import time
+import io
 
 
 base_url = "https://www.gross-electronic.de/"
@@ -40,24 +42,53 @@ def get_login_redirect_url(login_form_response):
     return redirect_url
 
 
+def get_download_area_url(after_login_html):
+    bs = BeautifulSoup(after_login_html, "html.parser")
+    csv_link = bs.find("a", string="Downloadbereich CSV")
+    csv_url = csv_link["href"]
+    return urljoin(base_url, csv_url)
+
+
+def get_export_area_url(download_area_html):
+    bs = BeautifulSoup(download_area_html, "html.parser")
+    export_link = bs.find(
+        "a", string="Artikelexport/Datenbank für Ihren Webshop als txt oder csv Datei"
+    )
+    export_url = export_link["href"]
+    return urljoin(base_url, export_url)
+
+
+def get_export_url(export_area_html):
+    bs = BeautifulSoup(export_area_html, "html.parser")
+    export_frame = bs.find("frame", attrs={"name": "export"})
+    export_url = export_frame["src"]
+    return urljoin(urljoin(base_url, "/exportfile/"), export_url)
+
+
+def get_csv_url(export_html):
+    bs = BeautifulSoup(export_html, "html.parser")
+
+    def csv_link_identifier(tag):
+        if not tag.name == "a":
+            return False
+        if (
+            "Exportdatei .CSV im Excel-Format downloaden (bitte hier klicken)"
+            in tag.strings
+        ):
+            return True
+        return False
+
+    csv_link = bs.find(csv_link_identifier)
+    csv_url = csv_link["href"]
+    return urljoin(urljoin(base_url, "/exportfile/"), csv_url)
+
+
 class GrossElectronic:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.request_session = requests.Session()
-        self.default_headers = {"User-Agent": UserAgent().random}
-
-    def get_login_html(self):
-        """Returns the html code of the login page."""
-        res = self.request_session.get(base_url, headers=self.default_headers)
-
-        if not res.ok:
-            raise Exception(
-                f"Could not connect to url {base_url}. "
-                f"Status code: {res.status_code}, reason: {res.reason}"
-            )
-
-        return res.text
+        self.default_headers = {"User-Agent": UserAgent().firefox}
 
     def send_login_form(self, login_html):
         """
@@ -85,17 +116,46 @@ class GrossElectronic:
 
         return res.text
 
-    def get_after_login_html(self, login_redirect_url):
+    def get_response_text(self, url):
         """
-        Given the redirect url, this method retrieves the html of the after login page.
+        Makes a get request to url using the request_session.
+        Returns the body of the response.
         """
 
-        res = self.request_session.get(login_redirect_url, headers=self.default_headers)
+        res = self.request_session.get(url, headers=self.default_headers)
 
         if not res.ok:
             raise Exception(
-                f"Error while getting after login html from {login_redirect_url}. "
+                f"Error while getting html for url {url}. "
                 f"Status code: {res.status_code}, reason: {res.reason}"
             )
 
         return res.text
+
+    def get_csv_file(self):
+        login_html = self.get_response_text(base_url)
+        time.sleep(1)
+
+        login_form_response = self.send_login_form(login_html)
+        redirect_url = get_login_redirect_url(login_form_response)
+        time.sleep(1)
+
+        after_login_html = self.get_response_text(redirect_url)
+        download_area_url = get_download_area_url(after_login_html)
+        time.sleep(1)
+
+        download_area_html = self.get_response_text(download_area_url)
+        export_area_url = get_export_area_url(download_area_html)
+        time.sleep(1)
+
+        export_area_html = self.get_response_text(export_area_url)
+        export_url = get_export_url(export_area_html)
+        time.sleep(1)
+
+        export_html = self.get_response_text(export_url)
+        csv_url = get_csv_url(export_html)
+        time.sleep(1)
+
+        csv_file = self.get_response_text(csv_url)
+
+        return io.StringIO(csv_file)
